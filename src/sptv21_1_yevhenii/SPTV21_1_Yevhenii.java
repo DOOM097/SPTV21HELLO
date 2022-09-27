6dbe0d98044347d368c52b38f0f6a5609eb9e216
Yevhenii Nemchenko <98549250+DOOM097@users.noreply.github.com>
2022-09-27 09:36:03 +0300

    Add files via upload

---
+++ src/sptv21_1_yevhenii/SPTV21_1_Yevhenii.java
@@ -0,0 +1,13 @@
+
+package sptv21_1_yevhenii;
+
+
+public class SPTV21_1_Yevhenii {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+     
+    }
+    
+}
